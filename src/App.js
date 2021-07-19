a9b77bf16fa292778111f610ca6f6a1577bf68cd
tonyykosseifyy <tonykosseify123@gmail.com>
2021-07-19 13:44:53 +0300

    importing fonts

--- src/App.js
+++ src/App.js
@@ -12,3 +12,27 @@ function App() {
 
 
 export default App ;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+- Strong cyan: hsl(172, 67%, 45%)
+
+- Very dark cyan: hsl(183, 100%, 15%)
+- Dark grayish cyan: hsl(186, 14%, 43%)
+- Dark grayish cyan: hsl(184, 14%, 56%)
+- Light grayish cyan: hsl(185, 41%, 84%)
+- Light grayish cyan: hsl(189, 41%, 97%)
+- White: hsl(0, 0%, 100%)
+
+- Font size (form inputs): 24px
+*/
